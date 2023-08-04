9d3129919bb68009f5b172f8b5d3b680293dbc45
haris-75 <haris.ejaz@datumbrain.com>
2023-08-04 23:26:14 +0500

    Add Header Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import Logo from './assets/logo.svg';
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
+    <section className='p-6 pt-20'>
+      <header className='flex flex-col gap-3 justify-center items-center'>
+        <div>
+          <img
+            src={Logo}
+            alt='logo-look-like-an-eye-and-letter-o'
+          />
+        </div>
+        <p>PNFT Market</p>
+      </header>
+      <main className='mt-12'>
+        <div></div>
+        <div>
+          <h1 className='text-3xl font-bold font-inter text-secondary'>
+            NFT Access
+          </h1>
+          <p className='font-inter mt-2 text-dark'>
+            Please fill your detail to access your account.
+          </p>
+        </div>
+      </main>
+    </section>
   );
 }
 
